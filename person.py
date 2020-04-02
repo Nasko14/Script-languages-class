36300e2200006636aa1ba9b876322095736b35f1
0DakatA0 <dstoilov199@gmail.com>
2020-04-02 18:50:44 +0300

    task compleated

--- person.py
+++ person.py
@@ -1,24 +1,45 @@
+import datetime
+
 class Person:
-    def __init__(self, name, age):
-        self.age = age
+    def __init__(self, name, year, mounth, day):
+        self.year = year
         self.name = name
+        self.mounth = mounth
+        self.day = day
 
     def __add__(self, other_obj):
-        self.p = Person("Dragan", self.age + other_obj.age)
-        print(self.p.name, self.p.age)
+        self.p = Person("Dragan", self.year + other_obj.year)
+        print(self.p.name, self.p.year)
         return self.p
 
     def __sub__(self, other_obj):
-        return self.age - other_obj.age
+        return self.year - other_obj.year
     
     def __mul__(self, other_obj):
-        return self.age * other_obj.age
+        return self.year * other_obj.year
     
-    def __radd__(self, age):
-        print(age)
-        return self.age + age
+    def __radd__(self, year):
+        print(year)
+        return self.year + year
+
+    def age(self):
+        d = datetime.date.today()
+        years = d.year - self.year
+
+        if self.mounth < d.month:
+            years -= 1
+
+        elif self.mounth == d.month:
+            if self.day > d.day:
+                print()
+                years -= 1
+
+        return years
 
 
-p1 = Person("Goshko", 15)
-p2 = Person("Ivan", 14)
-print(p1 + p2)
+
+p1 = Person("Goshko", 2004, 4, 8)
+#p2 = Person("Ivan", 14)
+#print(p1 + p2)
+
+print(p1.age())
